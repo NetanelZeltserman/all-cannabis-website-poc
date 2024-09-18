6c33cf89c79fc6e5f725c6af8c9eb13a01f077f1
NatiNati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-09-18 22:20:43 +0300

    chore: modify hero section title

--- src/frontend/src/sections/hero-section.tsx
+++ src/frontend/src/sections/hero-section.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
     <div className="w-full h-full grid container lg:grid-cols-2 items-center justify-items-center gap-5 my-12">
         <div className="order-2 lg:order-1 flex flex-col justify-center items-start">
             <p className="text-4xl font-medium md:text-7xl text-accentLight">קנאביס רפואי</p>
-            <p className="text-4xl font-medium text-secondary md:text-7xl">איפה, איך, מתי?</p>
+            <p className="text-4xl font-medium text-secondary md:text-7xl">איפה, בכמה, מתי?</p>
             <div className="space-y-2 md:space-y-0 md:space-x-4 md:rtl:space-x-reverse">
                 <Button variant="primary">גלה ממש פה!</Button>
                 <Button variant="secondary">ספר לי עוד</Button>
