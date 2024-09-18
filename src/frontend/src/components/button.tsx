db24c420ca2c9a55981b819a271c2ef815bc85b5
NatiNati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-09-18 22:49:00 +0300

    chiore: change primary btn syle

--- src/frontend/src/components/button.tsx
+++ src/frontend/src/components/button.tsx
@@ -9,8 +9,8 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export default function Button({ variant, onClick, children, className, ...props }: ButtonProps) {
   const baseClasses = "text-lg rounded-full md:text-2xl py-3 px-8 mt-10 transition duration-300 ease-in-out";
   const variantClasses = {
-    primary: "bg-primary hover:bg-primary-dark text-white",
-    secondary: "border-2 border-secondary hover:bg-secondary-dark text-secondary hover:text-white",
+    primary: "bg-accentLight hover:bg-accentLightDark text-white ring-2 ring-accentLight hover:ring-accentLightDark focus:ring-4 focus:ring-accentLight/50",
+    secondary: "border-2 border-secondary hover:bg-secondary-dark text-secondary hover:text-white hover:ring-2 hover:ring-secondary-dark focus:ring-4 focus:ring-secondary/50",
   };
 
   return (
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         'secondary-dark': '#4A5568',
         accent: '#597E6B',
         accentLight: '#397E2E',
+        accentLightDark: '#2E6425',
         accentDark: '#3C5447',
         textDark: '#1E2936',
         textLight: '#000101',
