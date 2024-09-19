54146856b4bf71ab43e7a2137310cc505292f5a3
Nati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-09-19 19:09:21 +0000

    chore: improve title design, add cannabis green colors

--- src/frontend/src/sections/hero-section.tsx
+++ src/frontend/src/sections/hero-section.tsx
@@ -4,8 +4,8 @@ export default function HeroSection() {
   return (
     <div className="w-full h-full grid container lg:grid-cols-2 items-center justify-items-center gap-5 my-12">
         <div className="order-2 lg:order-1 flex flex-col justify-center items-start">
-            <p className="text-4xl font-medium md:text-7xl text-accentLight">קנאביס רפואי</p>
-            <p className="text-4xl font-medium text-secondary md:text-7xl">איפה, בכמה, מתי?</p>
+            <p className="text-4xl font-medium md:text-7xl bg-gradient-to-r from-cannabisGreen-400 to-cannabisGreen-500 text-transparent bg-clip-text">קנאביס רפואי</p>
+            <p className="text-4xl font-medium text-cannabisGreen-500 md:text-7xl">איפה, בכמה, מתי?</p>
             <div className="space-y-2 md:space-y-0 md:space-x-4 md:rtl:space-x-reverse">
                 <Button variant="primary">גלה ממש פה!</Button>
                 <Button variant="secondary">ספר לי עוד</Button>
