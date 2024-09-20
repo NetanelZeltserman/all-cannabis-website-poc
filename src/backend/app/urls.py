aa269b42b0ee589635aed3f4d15eba88e408fb5e
NatiNati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-09-20 12:27:38 +0300

    feat: add get products endpoint

--- src/backend/app/urls.py
+++ src/backend/app/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
 from .views.products.get_featured import GetFeaturedProducts
+from .views.products.get import GetProducts
+from django.urls import path
 
 urlpatterns = [
     path('products/featured/', GetFeaturedProducts.as_view(), name='get_featured_products'),
+    path('products/', GetProducts.as_view(), name='get_products'),
+
 ]
